da59859f86d6d82467cbdb53229ee18085317db5
Jeam Alves <jeanmalves@gmail.com>
2018-03-05 11:47:08 -0300

    Adicionado configuração do seridor; Adicionado rota padrão; Adicionado rota para pesquisa de cep e por endereço; Adicionado get para pesquisa de cep e endereço.

---
+++ src/app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+const port = process.env.PORT || 8000;
+
+var router  = express.Router();
+
+const endPoint = 'https://viacep.com.br/ws';
+const returnType = 'json';
+
+// routes
+router.get('/', function(req, res) {
+  res.json({ message: 'YEAH! Seja Bem-Vindo a nossa API' });
+});
+
+router.route('/cep/:cep')
+  .get((req, res) => {
+    var cep = req.params.cep;
+    var url = `${endPoint}/${cep}/${returnType}`;
+
+    request(url, (error, response, body) => {
+        if(error || !body)
+            res.send(error)
+        const endereco = JSON.parse(body);
+        res.json(endereco);
+    })
+  });
+ 
+router.route('/address/:uf/:city/:street')
+  .get((req, res) => {
+    var uf = req.params.uf;
+    var city = req.params.city;
+    var street = req.params.street;
+    var url = `${endPoint}/${uf}/${city}/${street}/${returnType}`;
+
+    request(url, (error, response, body) => {
+        if(error || !body)
+            res.send(error)
+
+        if (response.statusCode === 200) {
+          const cep = JSON.parse(body);
+          res.json(cep);
+        }
+
+        if (response.statusCode === 400) {
+          res.status(response.statusCode).send({
+            error: 'A cidade e o logradouro precisam ter no mínimo 3 caracteres.'
+          }); 
+        }
+
+        res.status(response.statusCode).send({ error: `${response.statusCode}`}); 
+    });
+  });
+
+
+
+
+// prefixa as rotas com o valor /api
+app.use('/api', router);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+  console.log('To shutdown the server: ctrl + c');
+})
